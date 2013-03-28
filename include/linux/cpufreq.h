9e2a6f6300d458dd83311910af2577d2d61c5507
john craig <johnccraig80@gmail.com>
2013-03-28 08:55:42 -0400

    add badass and wheatley
    Change-Id: I03b91941edda273068ca79ee06c1132a0cd99ad2

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ASSWAX)
 extern struct cpufreq_governor cpufreq_gov_asswax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_asswax)
+#elif defined(CONFIG_CPU_FREQ_CPU_FREQ_DEFAULT_GOV_BADASS)
+extern struct cpufreq_governor cpufreq_gov_badass;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_badass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
 #endif
 
 
