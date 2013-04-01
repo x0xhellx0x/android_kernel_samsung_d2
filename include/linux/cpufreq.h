567cad04cffe16b8179ed9736c84b58b127ebf3d
john craig <johnccraig80@gmail.com>
2013-03-31 23:29:28 -0400

    Revert "cpufreq_intellidemand from faux" /home/john/jellybean/kernel/samsung/d2/arch/arm/mach-msm/cpufreq.c: In function 'msm_cpufreq_target': /home/john/jellybean/kernel/samsung/d2/arch/arm/mach-msm/cpufreq.c:150:2: error: implicit declaration of function 'cpufreq_frequency_get_table' [-Werror=implicit-function-declaration] /home/john/jellybean/kernel/samsung/d2/arch/arm/mach-msm/cpufreq.c:150:8: warning: assignment makes pointer from integer without a cast [enabled by default] error, forbidden warning: cpufreq.c:150 make[3]: *** [arch/arm/mach-msm/cpufreq.o] Error 1 make[2]: *** [arch/arm/mach-msm] Error 2 make[1]: *** [sub-make] Error 2
    This reverts commit b3d7cf1da7e2b53f78ff8d768eaed8425493f8d5.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -385,9 +385,6 @@ extern struct cpufreq_governor cpufreq_gov_badass;
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
 #endif
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
-extern struct cpufreq_governor cpufreq_gov_intellidemand;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_intellidemand)
 
 
 /*********************************************************************
