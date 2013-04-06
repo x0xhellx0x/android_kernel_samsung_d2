cf093d53fc546a16721fb6560ab8e0efebb598ab
john craig <johnccraig80@gmail.com>
2013-04-05 21:18:30 -0400

    intellidemand take 3
    Change-Id: Ifa802ebc61a177f7a34298315ec19d7cd7141e68

--- arch/arm/mach-msm/board-m2_att.c
+++ arch/arm/mach-msm/board-m2_att.c
@@ -181,6 +181,10 @@
 #include <asm/kexec.h>
 #endif
 
+#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+int id_set_two_phase_freq(int cpufreq);
+#endif
+
 extern unsigned int system_rev;
 #ifdef CONFIG_TOUCHSCREEN_MMS144
 struct tsp_callbacks *charger_callbacks;
@@ -5236,6 +5240,9 @@ static void __init msm8960_tsens_init(void)
 
 static void __init samsung_m2_att_init(void)
 {
+#ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND
+	id_set_two_phase_freq(1134000);
+#endif
 #ifdef CONFIG_SEC_DEBUG
 	sec_debug_init();
 #endif
