959d0089e683d73e5a3afc3e86d9280e89b951b3
john craig <johnccraig80@gmail.com>
2013-04-11 13:03:21 -0400

    oops
    Change-Id: I5539e2e79da46bcceab0c05c97bd603ce653667a

--- block/row-iosched.c
+++ block/row-iosched.c
@@ -86,23 +86,13 @@ struct row_queue_params {
  */
 static const struct row_queue_params row_queues_def[] = {
 /* idling_enabled, quantum, is_urgent */
-<<<<<<< HEAD
-	{true, 10, true},	/* ROWQ_PRIO_HIGH_READ */
-	{false, 1, false},	/* ROWQ_PRIO_HIGH_SWRITE */
-	{true, 100, true},	/* ROWQ_PRIO_REG_READ */
-	{false, 1, false},	/* ROWQ_PRIO_REG_SWRITE */
-	{false, 1, false},	/* ROWQ_PRIO_REG_WRITE */
-	{false, 1, false},	/* ROWQ_PRIO_LOW_READ */
-	{false, 1, false}	/* ROWQ_PRIO_LOW_SWRITE */
-=======
-	{true, 100, true},	/* ROWQ_PRIO_HIGH_READ */
-	{false, 4, true},	/* ROWQ_PRIO_HIGH_SWRITE */
-	{true, 75, true},	/* ROWQ_PRIO_REG_READ */
-	{false, 3, false},	/* ROWQ_PRIO_REG_SWRITE */
-	{false, 3, false},	/* ROWQ_PRIO_REG_WRITE */
-	{false, 3, false},	/* ROWQ_PRIO_LOW_READ */
-	{false, 2, false}	/* ROWQ_PRIO_LOW_SWRITE */
->>>>>>> 447b554... block: row: Some good tweaks from around the forum
+	{true, 100, true},  /* ROWQ_PRIO_HIGH_READ */
+	{false, 4, true},  /* ROWQ_PRIO_HIGH_SWRITE */
+	{true, 75, true},  /* ROWQ_PRIO_REG_READ */
+	{false, 3, false},  /* ROWQ_PRIO_REG_SWRITE */
+	{false, 3, false},  /* ROWQ_PRIO_REG_WRITE */
+	{false, 3, false},  /* ROWQ_PRIO_LOW_READ */
+	{false, 2, false}  /* ROWQ_PRIO_LOW_SWRITE */
 };
 
 /* Default values for idling on read queues (in msec) */
